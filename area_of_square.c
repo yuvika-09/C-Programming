066690bb54f6c47ab6e7c2f42b689dfc00883886
Yuvika <140506905+yuvika-09@users.noreply.github.com>
2024-03-22 21:15:17 +0530

    Create area_of_square.c

---
+++ area_of_square.c
@@ -0,0 +1,10 @@
+//calculate area of square when side is input by user
+
+#include<stdio.h>
+void main(){ 
+	float a,r;
+	printf("Enter the Radius : ");
+	scanf("%f",&s);
+	a=s*s;
+	printf("Area is %f",a);
+}
