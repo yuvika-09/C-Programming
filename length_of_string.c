e5e989ab4827260c43c6b9cfbd8d033e61396c32
Yuvika <140506905+yuvika-09@users.noreply.github.com>
2024-04-19 21:38:55 +0530

    Create length_of_string.c

---
+++ length_of_string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<strings.h>
+void main(){
+    char s[30],a;
+    int len=0;
+    scanf("%[^\n]s",&s);
+    strlwr(s);
+    for(int i=0;s[i]!='\0';i++){
+        len++;
+    }
+    printf("Length of string : %d",len);
+}
