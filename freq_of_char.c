38b7ddb893c63d1b8bd0e6f2cd4a3e123c9578f5
Yuvika <140506905+yuvika-09@users.noreply.github.com>
2024-04-19 21:37:53 +0530

    Create freq_of_char.c

---
+++ freq_of_char.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    char s[30],a;
+    int c=0;
+    scanf("%[^\n]s",&s);
+    printf("Character you want to count: ");
+    scanf(" %c",&a);
+    for(int i=0;i<30;i++){
+        if(s[i]==a)
+            c++;
+    }
+    printf("Frequency of %c = %d",a,c);
+}
