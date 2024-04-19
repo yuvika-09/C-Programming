d1bbb110826547746e1b5bba4293c2e4e4ae1f8a
Yuvika <140506905+yuvika-09@users.noreply.github.com>
2024-04-19 21:50:57 +0530

    Update array_insertion.c

--- array_insertion.c
+++ array_insertion.c
@@ -2,12 +2,16 @@
 
 #include<stdio.h>
 int main(){
-    int i, A[5];
-    for (i=0;i<5;i++){
+    int i,n;
+    printf("Enter the no of elements you want in your array : ");
+    scanf("%d",&n);
+    int A[n];
+    for (i=0;i<n;i++){
         printf("Enter value at A[%d] : ",i+1);
         scanf("%d",&A[i]);
     }
-    for (i=0;i<5;i++){
+    //display the array
+    for (i=0;i<n;i++){
         printf("\nValue of element A[%d] = %d",i+1,A[i]);
     }
 }
