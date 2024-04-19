b56eba09e76bcf1e246402fab00d5a249e127fd5
Yuvika <140506905+yuvika-09@users.noreply.github.com>
2024-04-19 21:52:59 +0530

    Update array_deletion.c

--- array_deletion.c
+++ array_deletion.c
@@ -2,18 +2,26 @@
 
 #include<stdio.h>
 int main(){
-    int i,loc,A[4]={31,22,97,67};
+    int i,loc,n;
+    printf("Enter the no of elements you want in your array : ");
+    scanf("%d",&n);
+    int A[n];
+    for (i=0;i<n;i++){
+        printf("Enter value at A[%d] : ",i+1);
+        scanf("%d",&A[i]);
+    }
     printf("Enter location to delete element : ");
     scanf("%d",&loc);
-    if(loc>=4){
+    if(loc>=n){
         printf("Location doesnot exist");
     }
     else{
-        for (i=loc-1;i<4;i++){
+        for (i=loc-1;i<n;i++){
             A[i] = A[i+1];
         }
-        for (i=0;i<3;i++){
+        for (i=0;i<n-1;i++){
             printf("\nValue of element A[%d] = %d",i+1,A[i]);
         }
     }
+    return 0;
 }
